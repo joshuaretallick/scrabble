368e113e4a237026473eb3a64b73d919602deb63
Joshua Retallick <josh@old-empire.co.uk>
2020-12-29 12:01:56 +0000

    Tidying up, cant get last test to pass if string is nil

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -2,17 +2,14 @@ require 'scrabble'
 
 describe "#scrabble" do
   it "returns a score of one when inputted 'a'" do
-    # "a"       |     "1"
     expect(scrabble("a")).to eq 1
   end
 
   it "returns a score of 0 when no input" do
-    # ""        |     "0"
     expect(scrabble("")).to eq 0
   end
 
   xit "returns a score of 0 if input is not string" do
-    # a         |     "0"
     expect(scrabble(a)).to eq 0
   end
 
